7ee5caf616b2ff6ccf29f14420b244ca17ec38fa
SofiaPinilla <contact@sofiapinilla.com>
2022-04-11 10:48:17 +0200

    primera clase js

---
+++ main.js
@@ -0,0 +1,55 @@
+// const pedro = 10;
+//     let age2 ='20'
+//     let age = 20
+// console.log('Tengo ' + age + ' años');
+// console.log( `Tengo ${age} años`)
+
+// const s = "Hello World";
+// let val;
+// val = s.length;
+
+// val = s.toUpperCase();
+// val = s.toLowerCase();
+// val = s.substring(0,5);
+// val = s.split("",3);
+// console.log(val)
+// const numbers = [1,2,3,7,5];
+// const fruits = ['apples', 'oranges', 'pears', 'grapes'];
+
+// console.log(fruits[2])
+
+// const fruits = ["apples", "oranges", "pears", "grapes"];
+
+// // fruits[5] = "blueberries";
+// fruits[4] = "blueberries";
+
+// fruits.push("strawberries");
+
+// fruits.unshift("mangos");
+
+// fruits.pop();
+
+// console.log(fruits.indexOf("oranges"));
+// let newFruit = fruits.pop()
+// console.log(fruits)
+// console.log(newFruit);
+
+const person = {
+  name: "Edu",
+  age: 30,
+  hobbies: [
+    "music",
+    "movies",
+    {
+      sports: ["futbol", "tenis", "ajedrez", "lol"],
+    },
+  ],
+  address: {
+    street: "50 Main st",
+    city: "Boston",
+    state: "MA",
+  },
+};
+person.email = "jdoe@gmail.com";
+console.log(person)
+// console.log(person.hobbies[2].sports[3]);
